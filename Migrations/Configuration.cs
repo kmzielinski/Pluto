cb26936d955716855896bc1b04b43efcc9ca675a
Krzysztof Zielinski <kmzielinski@gmail.com>
2017-04-06 22:30:59 +0200

    Initial commmit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,43 @@
+namespace CodeFirstExistingDatabase.Migrations
+{
+    using CodeFirstExistingDatabase;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstExistingDatabase.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstExistingDatabase.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName, 
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.Authors.AddOrUpdate(a => a.Name,
+               new Authors
+               {
+                   Name = "Author 1",
+                   Courses = new Collection<Courses>()
+                   {
+                       new Courses () { Name = "Course for Author1", Description = "Description" }
+                   }
+               });
+        }
+    }
+}
